0bd3f6953d34a3e9005f96ddd25b721c9a971204
Alex Barbero <barrufat@hotmail.com>
2023-10-12 20:23:13 +0200

    Create array

---
+++ Array.js/Array.js
@@ -0,0 +1,9 @@
+class Array{
+  array;
+  length;
+
+  constructor(...data){
+    this.length = data.length;
+    this.array = data;
+  }
+}
---
+++ Array.js/index.js
@@ -0,0 +1,8 @@
+class Prueva {
+  array;
+  length;
+
+  constructor(...data) {
+    this.length = data.length;
+    this.array = data;
+  }
