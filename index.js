5c7efa3577631c23ecd6ea51fa6196081898fb79
Alex Barbero <barrufat@hotmail.com>
2023-10-12 23:52:17 +0200

    Add find & some methods

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import Array from "./Array.js/Array.js";
 
 export const numbers = new Array(3, 8, 10, 2);
 
-console.log(numbers.length);
+console.log(numbers.map((number) => number + 1));
+
+console.log(numbers.filter((number) => number < 10));
+
+console.log(numbers.find((number) => number < 10));
